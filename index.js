b11e18e541d3b5de70163df75b1c724429e68898
Clément FAZILLEAU <c.fazilleau@student.isartdigital.com>
2021-05-30 16:24:37 +0200

    [Add] setting to skip or cancel on already existing file

--- index.js
+++ index.js
@@ -1,61 +1,103 @@
-const Discord = require(`discord.js`);
-const https = require(`https`);
-const fs = require(`fs`);
-const settings = require(`./config.json`);
+const Discord 	= require("discord.js");
+const https 	= require("https");
+const fs 		= require("fs");
+
+const settings 	= require("./config.json");
 
 const client = new Discord.Client();
 
-const getContent = function (url, path) {
-    return new Promise((resolve, reject) => {
-        const request = https.get(url, response => {
-            if (response.statusCode < 200 || response.statusCode > 299) {
-                reject(new Error('Failed to load page, status code: ' + response.statusCode));
-            }
-            response.pipe(fs.createWriteStream(path))
-                .on('finish', () => resolve())
-                .on('error', e => reject(e));
-        });
-        request.on('error', e => reject(e))
-    })
+const getContent = function (url, path)
+{
+	return new Promise((resolve, reject) =>
+	{
+		const request = https.get(url, response =>
+		{
+			if (response.statusCode < 200 || response.statusCode > 299)
+			{
+				reject(new Error('Failed to load resource, status code: ' + response.statusCode));
+			}
+		
+			response.pipe(fs.createWriteStream(path))
+				.on('finish', () => resolve())
+				.on('error', e => reject(e));
+		});
+		
+		request.on('error', e => reject(e))
+	});
 };
 
-const saveMedias = async msg => {
-    var lastMsg = msg.id;
-    var batchSize = settings.defaultBatchSize;
-    var amount = 0;
+const saveMedias = async function(msg)
+{
+	var lastMsg		= msg.id;
+	var batchSize	= settings.defaultBatchSize;
+	var amount		= 0;
+	var done		= false;
 
-    msg.channel.send(`saving...`);
-    console.log(`saving...`);
+	msg.channel.send("saving...");
+	console.log("saving...");
 
-    const dir = `./saved/` + msg.channel.id + `/`
-    fs.mkdirSync(dir, { recursive: true });
+	// Create save folder
+	const dir = "./saved/" + msg.channel.id + "/"
+	fs.mkdirSync(dir, { recursive: true });
 
-    while (batchSize == settings.defaultBatchSize) {
-        const batch = await msg.channel.fetchMessages({ limit: settings.defaultBatchSize, before: lastMsg });
-        batchSize = batch.size;
-        console.log(`fetched ` + batchSize + ` messages.`);
-        for await (var element of batch.values()) {
-            for await (var attachment of element.attachments.values()) {
-                const filename = element.createdTimestamp + `_` + attachment.url.split(`/`).pop();
-                await getContent(attachment.url, dir + filename);
-                console.log(++amount + `. saved "` + filename + `".`);
-            }
-        }
+	while (batchSize == settings.defaultBatchSize && !done)
+	{
+		// Fetch Messages batch
+		const batch = await msg.channel.fetchMessages({ limit: settings.defaultBatchSize, before: lastMsg });
+		batchSize = batch.size;
+		console.log("fetched " + batchSize + " messages.");
+		
+		// Iterate on messages
+		for await (var element of batch.values())
+		{
+			// Iterate on attachments
+			for await (var attachment of element.attachments.values())
+			{
+				const filename = element.createdTimestamp + "_" + attachment.url.split("/").pop();
 
-        if (batchSize > 0)
-            lastMsg = batch.last().id;
-    }
-    msg.channel.send(`done! successfully saved ` + amount + ` attachments.`);
-    console.log(`done! successfully saved ` + amount + ` attachments.`);
+				// If the content has already been saved, skip or cancel
+				if (fs.existsSync(dir + filename))
+				{
+					if (settings.cancelOnAlreadyExists)
+					{
+						console.log(filename + " already exists, skipping save.");
+						done = true;
+						break;
+					}
+					else if (settings.skipOnAlreadyExists)
+					{
+						console.log(filename + " already exists, skipping save.");
+						continue;
+					}
+					else
+					{
+						console.log("overwriting " + filename + ".");
+					}
+				}
+
+				// Write file
+				await getContent(attachment.url, dir + filename);
+				console.log(++amount + ". saved \"" + filename + "\"");
+			}
+		}
+
+		// Continue fetching messages before the date of the first element of the previous batch
+		if (batchSize > 0)
+			lastMsg = batch.last().id;
+	}
+
+	// Done saving messages
+	msg.channel.send("done! successfully saved " + amount + " attachments.");
+	console.log("done! successfully saved " + amount + " attachments.");
 }
 
-client.on(`ready`, () => {
-    console.log(`Logged in as ${client.user.tag}! join with https://discordapp.com/api/oauth2/authorize?client_id=${settings.botId}&scope=bot&permissions=65536`);
+client.on("ready", () => {
+	console.log(`Logged in as ${client.user.tag}! join with https://discordapp.com/api/oauth2/authorize?client_id=${settings.botId}&scope=bot&permissions=65536`);
 });
 
-client.on(`message`, msg => {
-    if (msg.content == settings.saveTrigger && msg.author.id === settings.authorizedUserId)
-        saveMedias(msg);
+client.on("message", msg => {
+	if (msg.author.id === settings.authorizedUserId && msg.content == settings.saveTrigger)
+		saveMedias(msg);
 });
 
 client.login(settings.botToken).catch(console.error);
--- readme.md
+++ readme.md
@@ -2,12 +2,11 @@
 is a small bot I made to save all the pictures sent in a discord channel.
 
 It uses the packages
-- discord.js to access to the messages.
-- https to handle the web requests.
-- fs to save the images to a local directory.
+- discord.js to access the messages.
+- https to download attachments using web requests.
+- fs to save images to a local directory.
 
 # How to use ?
-
 you need to install the dependencies using npm (`npm install`)
 and create a `config.json` file:
 
@@ -17,6 +16,8 @@ and create a `config.json` file:
     "saveTrigger": "YOUR_SAVE_COMMAND",
     "botToken": "YOUR_BOT_TOKEN",
     "botId": "YOUR_BOT_ID",
-    "defaultBatchSize": 100
+    "defaultBatchSize": 100,
+    "cancelOnAlreadyExists": true,
+    "skipOnAlreadyExists": false
 }
 ```
